5adee70c86a7b0e10a2a80ad5b6bc534a1022615
Sanjay Sharma <sanjay.sharma@nearmap.com>
2019-07-14 22:25:14 +1000

    12 seconds! But still not fast enough for HR

--- KingRichardsKnights/Program.cs
+++ KingRichardsKnights/Program.cs
@@ -25,9 +25,7 @@ namespace KingRichardsKnights
          */
         static int[][] kingRichardKnights(int n, int s, int[][] commands, int l, int[] knights)
         {
-            int a = 1;
-            int b = 1;
-            int d = n - 1;
+            
             var counter = 1;
             var locations = new List<KnightLocation>();
             locations.AddRange(
@@ -39,9 +37,6 @@ namespace KingRichardsKnights
                             counter = 0
                         }));
 
-            var allLocations = new List<KnightLocation>();
-            allLocations.AddRange(locations);
-
             var groupedCommands = commands
                 .Select(c => new {a = c[0], b = c[1], d = c[2]})
                 .GroupBy(x => new
@@ -52,100 +47,32 @@ namespace KingRichardsKnights
 
             foreach (var knight in locations)
             {
+                int a = 1;
+                int b = 1;
+                int d = n - 1;
+
+                var row = knight.location[0];
+                var col = knight.location[1];
+
                 foreach (var command in groupedCommands)
                 {
-                    if (InRange(knight.location[0],
-                        knight.location[1],
-                        command.a,
-                        command.b,
-                        command.d,
-                        n))
-                    {
-                        a = command.a;
-                        b = command.b;
-                        d = command.d;
+                    a = command.a;
+                    b = command.b;
+                    d = command.d;
 
-                        var row = knight.location[0];
-                        var col = knight.location[1];
-                        var subRow = row - (a - 1);
-                        var subCol = col - (b - 1);
+                    if (row - (a - 1) < 1 || col - (b - 1) < 1)
+                        break;
+                    if (row - (a - 1) > d + 1 || col - (b - 1) > d + 1)
+                        break;
 
-                        var temp = subCol;
-                        subCol = (d + 1) - subRow;
-                        subRow = temp;
-
-                        knight.location[0] = subRow + a - 1;
-                        knight.location[1] = subCol + b;
-                    }
+                    var oldRow = row;
+                    row = col - (b - 1) + a - 1;
+                    col = (d + 1) - (oldRow - (a - 1)) + b;
                 }
+
+                knight.location[0] = row;
+                knight.location[1] = col;
             }
-
-            //foreach (var command in groupedCommands)
-            //{
-            //    //if (command.a == a && command.b == b && command.d == d)
-            //    //{
-            //    //    continue;
-            //    //}
-
-            //    if (command.a < a || command.b < b || command.d > d ||
-            //        command.a + command.d > a + d || command.b + command.d > b + d)
-            //    {
-            //        continue;
-            //    }
-
-            //    a = command.a;
-            //    b = command.b;
-            //    d = command.d;
-
-            //    locations = locations
-            //        .GroupBy(x => x.knight)
-            //        .Select(x => x.OrderByDescending(g => g.counter).First())
-            //        .Where(x => InRange(x.location[0], x.location[1], a, b, d, n))
-            //        .Select(ckl => new KnightLocation
-            //        {
-            //            knight = ckl.knight,
-            //            location = new[]
-            //            {
-            //                //subRow + a - 1,
-            //                //subCol + b
-            //                ckl.location[1] - (b - 1) + a - 1,
-            //                (d + 1) - ckl.location[0] + (a - 1) + b
-            //            },
-            //            counter = counter
-            //        }).ToList();
-
-            //    counter++;
-
-            //    allLocations.AddRange(locations);
-
-
-
-
-               //foreach (var ckl in commandknightLocations)
-               //{
-               //    var row = ckl.location[0];
-               //    var col = ckl.location[1];
-               //    var subRow = row - (a - 1);
-               //    var subCol = col - (b - 1);
-
-                //    var temp = subCol;
-                //    subCol = (d + 1) - subRow;
-                //    subRow = temp;
-
-                //    knightLocations.Single(k => k.knight == ckl.knight)
-                //            .location[0] = subRow + a - 1;
-                //    knightLocations.Single(k => k.knight == ckl.knight)
-                //            .location[1] = subCol + b;
-
-                //}
-            
-
-            //allLocations = allLocations
-            //    .GroupBy(x => x.knight)
-            //    .Select(x => x.OrderByDescending(g => g.counter).First())
-            //    .ToList();
-            
-            
 
             var result = locations
                 .Select(x => x.location).ToArray();
@@ -164,7 +91,7 @@ namespace KingRichardsKnights
         {
             //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-            bool readfromFile = true;
+            bool readfromFile = false;
 
             if (readfromFile)
             {
@@ -232,70 +159,6 @@ namespace KingRichardsKnights
                 //textWriter.Flush();
                 //textWriter.Close();
             }
-
-
-        }
-
-        private static void RotateMatrix(int[] matrix, int a, int b, int d, ref int[] originalMatrix, int n,
-            ref int[] locationMatrix, int l, int[] knights)
-        {
-           
-
-            var nmatrix = new int[matrix.Length];
-
-            for (var key = 0; key < matrix.Length; key++)
-            {
-                var row = key / (d + 1);
-                var col = key % (d + 1);
-
-                var temp = col;
-                col = (d + 1) - row - 1;
-                row = temp;
-                nmatrix[row * (d + 1) + col] = matrix[key];
-                row = row + (a - 1);
-                col = col + b - 1;
-
-                originalMatrix[row * n + col] = matrix[key];
-                locationMatrix[matrix[key]] = row * n + col;
-            }
-        }
-
-        private static void InitMatrix(int N, int[] matrix)
-        {
-            for (int row = 1; row <= N; row++)
-            {
-                for (int col = 1; col <= N; col++)
-                {
-                    matrix[(row - 1) * N + col - 1] = (row - 1) * N + col - 1;
-                }
-            }
-        }
-
-        private static void PrintMatrix(int N, int[] matrix)
-        {
-            for (int row = 0; row < N; row++)
-            {
-                for (int col = 0; col < N; col++)
-                {
-                    //var key = InRange * N + colInRange;
-                    //Console.WriteLine($"{key} -> Row : " +
-                    //              $"{matrix[key].InRange} Col : " +
-                    //              $"{matrix[key].colInRange}");
-
-                    try
-                    {
-                        Console.Write(matrix[row * N + col] + " ");
-                    }
-                    catch (Exception e)
-                    {
-                        Console.Write("E ");
-
-                    }
-
-                }
-                Console.WriteLine();
-            }
-            Console.ReadKey();
         }
     }
 }
